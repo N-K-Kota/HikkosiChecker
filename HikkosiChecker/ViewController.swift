f6418330d09e6ef8c7466a3d5ddb60170ee747c8
konta <mmilefun@gmail.com>
2018-11-02 12:41:52 +0900

    Initial Commit

---
+++ HikkosiChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HikkosiChecker
+//
+//  Created by Kota Nakamura on 2018/11/02.
+//  Copyright © 2018年 Kota Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
